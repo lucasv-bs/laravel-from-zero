4711fec85a86077f229923a39879ee7ab15493df
Lucas Vinicius <lucasvinicius.bs03@gmail.com>
2022-03-26 04:23:57 -0300

    Add the database structure with models and migrations

---
+++ app/Models/Category.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Category extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    
+    protected $fillable = [
+        'name',
+        'image'
+    ];
+
+    protected $dates = [
+        'deleted_at'
+    ];
+
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+}
