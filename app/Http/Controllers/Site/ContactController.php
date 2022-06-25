deca3c00de2868971180f9d415fd64b2a9e004f9
Lucas Vinicius <lucasvinicius.bs03@gmail.com>
2022-06-25 02:49:05 -0300

    Add the contact form with initial basic working

--- app/Http/Controllers/Site/ContactController.php
+++ app/Http/Controllers/Site/ContactController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\Site;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Notification;
+
+use App\Models\Contact;
+use App\Notifications\NewContact;
 
 class ContactController extends Controller
 {
@@ -25,6 +29,9 @@ class ContactController extends Controller
      */
     public function send(Request $request)
     {
-        
+        $contact = Contact::create($request->all());
+
+        Notification::route('mail', config('mail.from.address'))
+            ->notify(new NewContact($contact));
     }
 }
